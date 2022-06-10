8fe9b28ad68a984c0bb0fee37c8012efbf1ec958
Victor <victor.maciel01@gmail.com>
2022-06-10 00:59:21 -0300

    Atualizacao home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,12 @@ import axios from '../axios'
 import Router from 'next/router'
 
 export default function Home() {
+    const pessoas = [
+        { nome: 'Mamadeira', value: 5 },
+        { nome: 'Educado', value: 10 },
+        { nome: 'Pi', value: -5 },
+    ]
+
     const [user, setUser] = useState('')
 
     useEffect(() => {
@@ -40,16 +46,48 @@ export default function Home() {
                         <p className="font-bold mt-4">Seus Grupos:</p>
                         <div className="flex">
                             <GroupCard name="AP 127" value={50} />
-                            <GroupCard name="AP 127" value={50} />
-                            <GroupCard name="AP 127" value={50} />
-                            <GroupCard name="AP 127" value={-30} />
-                            <GroupCard name="AP 127" value={-20} />
+                            <GroupCard name="AP 229" value={20} />
+                            <GroupCard name="Churras" value={-10.5} />
+                            <GroupCard name="UBER SP" value={-30} />
+                            <GroupCard name="Casd" value={1.5} />
                         </div>
                     </div>
                 </div>
                 <div className="h-full pt-8 -mr-40 right-0 w-2/5 bg-light-gray flex flex-col pl-8 pr-48 items-center">
                     <p className="text-lg font-bold text-primary">RESUMO</p>
-                    <div className="bg-background h-64 rounded-xl w-full my-8"></div>
+                    <div className="bg-background h-64 rounded-xl w-full my-8 py-4 px-6 flex flex-col items-center">
+                        <p className="font-semibold">Quem me deve</p>
+                        {pessoas
+                            .filter(elem => elem.value > 0)
+                            .map(elem => (
+                                <div className="flex mt-2 justify-between w-full">
+                                    <p>{elem.nome}</p>
+                                    <p className={`text-green-600 font-semibold`}>
+                                        {elem.value.toLocaleString('pt-br', {
+                                            style: 'currency',
+                                            currency: 'BRL',
+                                        })}
+                                    </p>
+                                </div>
+                            ))}
+                        <span className="text-primary mt-1 cursor-pointer hover:opacity-70 transition-opacity font-semibold text-xs w-full">
+                            Cobrar
+                        </span>
+                        <p className="font-semibold mt-6">Para quem eu devo</p>
+                        {pessoas
+                            .filter(elem => elem.value < 0)
+                            .map(elem => (
+                                <div className="flex mt-2 justify-between w-full">
+                                    <p>{elem.nome}</p>
+                                    <p className={`text-red-600 font-semibold`}>
+                                        {elem.value.toLocaleString('pt-br', {
+                                            style: 'currency',
+                                            currency: 'BRL',
+                                        })}
+                                    </p>
+                                </div>
+                            ))}
+                    </div>
                     <Button title="Ver Detalhes" style="text-sm" />
                 </div>
             </div>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import { Button } from '../components/Button'
 import { useState } from 'react'
 import Router from 'next/router'
+import Head from 'next/head'
 
 import axios from '../axios'
 
@@ -26,65 +27,70 @@ export default function Login() {
         setSenha('')
     }
     return (
-        <div className="flex h-full">
-            <div className="w-1/3 bg-primary"></div>
-            <div className="flex flex-grow mx-4 justify-center items-center">
-                <div className="w-full max-w-xl bg-white rounded-xl px-8 pt-6 pb-8 mb-4">
-                    <form className="" method="POST">
-                        <div className="mb-4">
-                            <label
-                                className="block text-gray-700 text-sm font-bold mb-2"
-                                htmlFor="email">
-                                Email
-                            </label>
-                            <input
-                                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-xl w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary transition-all"
-                                type="email"
-                                id="email"
-                                placeholder="Escreva seu email"
-                                value={email}
-                                onChange={event => setEmail(event.target.value)}
-                            />
-                        </div>
+        <>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <div className="flex h-full">
+                <div className="w-1/3 bg-primary"></div>
+                <div className="flex flex-grow mx-4 justify-center items-center">
+                    <div className="w-full max-w-xl bg-white rounded-xl px-8 pt-6 pb-8 mb-4">
+                        <form className="" method="POST">
+                            <div className="mb-4">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="email">
+                                    Email
+                                </label>
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-xl w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary transition-all"
+                                    type="email"
+                                    id="email"
+                                    placeholder="Escreva seu email"
+                                    value={email}
+                                    onChange={event => setEmail(event.target.value)}
+                                />
+                            </div>
 
-                        <div className="mb-6">
-                            <label
-                                className="block text-gray-700 text-sm font-bold mb-2"
-                                htmlFor="senha">
-                                Senha
-                            </label>
-                            <input
-                                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-xl w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary transition-all"
-                                type="password"
-                                id="senha"
-                                placeholder="Escreva sua senha"
-                                value={senha}
-                                onChange={event => setSenha(event.target.value)}
-                            />
-                            <a
-                                className="mt-1 ml-1 text-xs text-primary font-semibold hover:opacity-70 transition-opacity"
-                                href="/">
-                                Esqueceu sua senha?
-                            </a>
-                        </div>
+                            <div className="mb-6">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="senha">
+                                    Senha
+                                </label>
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-xl w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary transition-all"
+                                    type="password"
+                                    id="senha"
+                                    placeholder="Escreva sua senha"
+                                    value={senha}
+                                    onChange={event => setSenha(event.target.value)}
+                                />
+                                <a
+                                    className="mt-1 ml-1 text-xs text-primary font-semibold hover:opacity-70 transition-opacity"
+                                    href="/">
+                                    Esqueceu sua senha?
+                                </a>
+                            </div>
 
-                        <div className="w-full flex justify-center">
-                            <Button primary title="Login" onClick={logIn} />
-                        </div>
-                    </form>
+                            <div className="w-full flex justify-center">
+                                <Button primary title="Login" onClick={logIn} />
+                            </div>
+                        </form>
 
-                    <div className="w-full flex flex-col items-center justify-center text-sm pt-6">
-                        <div className="">
-                            Se ainda não tem conta, inscreva-se{' '}
-                            <a
-                                href="/signin"
-                                className="font-bold text-primary hover:opacity-70 transition-opacity">
-                                aqui
-                            </a>
+                        <div className="w-full flex flex-col items-center justify-center text-sm pt-6">
+                            <div className="">
+                                Se ainda não tem conta, inscreva-se{' '}
+                                <a
+                                    href="/signin"
+                                    className="font-bold text-primary hover:opacity-70 transition-opacity">
+                                    aqui
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
