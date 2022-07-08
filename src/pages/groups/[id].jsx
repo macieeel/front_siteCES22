d2c3a6e0166c662f337057d59c1b069da69787f1
Victor <victor.maciel01@gmail.com>
2022-07-08 14:41:01 -0300

    fixed error

--- src/pages/groups/[id].jsx
+++ src/pages/groups/[id].jsx
@@ -51,7 +51,7 @@ export default function Group() {
 
     return (
         <>
-            {group ? (
+            {!!group ? (
                 <>
                     <Head>
                         <title>{'Grupo | ' + group.nome}</title>
